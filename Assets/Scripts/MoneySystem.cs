abec271fbe6ee26cd1c633bcbce8c9bfab3d8d69
max <1223max@gmail.com>
2021-03-10 14:25:16 +0100

    money system
    rip my 4g

---
+++ Assets/Scripts/MoneySystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneySystem : MonoBehaviour
+{
+
+
+    public delegate int ChangeMoney(int newMoney);
+    ChangeMoney ChangeM;
+
+    public static int money = 0;
+    public Text moneytxt;
+    public Text totalTxt;
+    public static int newMoney;
+
+    private void Start()
+    {
+        ChangeM = AddMoney;
+    }
+    void Update()
+    {
+        moneytxt.text = "" + money;
+    }
+    private int AddMoney(int newMoney)
+    {
+        newMoney += money;
+        return money;
+    }
+  
+}
